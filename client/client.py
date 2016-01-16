bfbcb39cbe5c00954b411ca84517e34c1cd67e6d
Mitch <mstehula@gmail.com>
2016-01-15 23:30:35 -0500

    Changed a little typo in one of the prompts

--- client/client.py
+++ client/client.py
@@ -10,7 +10,10 @@ import sys
 print('Welcome to the first iteration of secure chats')
 
 username = str(input('Please enter a username to use: '))
-ch = CommHandler()
+localport = int(input('Please enter a port to start the server on: '))
+hostname = str(input('Please enter a host name to connect to: '))
+hostport = int(input('Please enter the port to target: '))
+ch = CommHandler(localport, hostname, hostport)
 ui = UI(username)
 
 print('Setup complete. Anything you type will be directly transmitted to the remote client')
