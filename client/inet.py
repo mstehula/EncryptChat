62fa877efe1e76566ad2a1fc0bdc472a0167f2e5
Mitch <mstehula@gmail.com>
2016-01-15 22:39:36 -0500

    Minor tweaks to get inet to function properly with he ui

--- client/inet.py
+++ client/inet.py
@@ -2,42 +2,46 @@ import select
 import socket
 import struct
 
-class CommunicationHandler():
-
-    client, listener = None
+class CommHandler():
 
     #Setup for the listener server socket and client socket. Also starts the
-    #server, but does not start accepting
-    def __init__:
-        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-        host = listener.gethostname()
+    #server, but does not start accepting. Currently listener is locked at port
+    #5555
+    def __init__(self):
+        self.listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        host = 'localhost'
         port = 5555
-        listener.bind((host, port))
-        listener.listen(5)
+        self.listener.bind((host, port))
+        self.listener.listen(5)
 
-        client = socket.socket()
+        self.client = socket.socket()
 
     #Sends message to the given socket as the given username.
-    def sendMessage(username, message):
+    def sendMessage(self, username, message):
+        self.client.connect(('localhost', 5555))
+
         length = len(message)
 
-        remote.send(struct.pack("64s", username.encode()))
-        remote.send(struct.pack("I", length))
-        remote.send(struct.pack('%ds' % length, message.encode()))
+        self.client.send(struct.pack("64s", username.encode()))
+        self.client.send(struct.pack("I", length))
+        self.client.send(struct.pack('%ds' % length, message.encode()))
+
+        self.client.close()
+        self.client = socket.socket()
 
     #Check to see if there is a message available for the listener to recv
-    def checkForMessage():
-        readable = select.select([listener],[],[],0)
+    def checkForMessage(self):
+        readable = select.select([self.listener],[],[],0)[0]
         return len(readable) != 0
 
     #Receive a Message on a given Socket. This is strictly the
     #message in the scope of this program, consisting of 64B name, 4B datalength,
     #data (up to) 2^32
-    def recvMessage():
-        recvsocket = listener.accept()[0]
-        username = struct.unpack('64s', recvsocket.recv(64))[0].replace(b'\0',b'')
+    def recvMessage(self):
+        recvsocket = self.listener.accept()[0]
+        username = struct.unpack('64s', recvsocket.recv(64))[0].replace(b'\0',b'').decode()
         size = struct.unpack('I', recvsocket.recv(4))[0]
-        data = struct.unpack('%ds' % size, recvsocket.recv(size))[0]
+        data = struct.unpack('%ds' % size, recvsocket.recv(size))[0].decode()
         recvsocket.close()
 
         return (username, data)
