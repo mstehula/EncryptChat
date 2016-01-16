2d971d7e281e4a68b647ef66794bd9b8b1cdcb96
Mitch <mstehula@gmail.com>
2016-01-15 20:20:05 -0500

    Refactored class ConnectionHandler to include server handling for startup. Changed send/recv to implement the connection of servers as well

--- client/inet.py
+++ client/inet.py
@@ -1,20 +1,43 @@
+import select
 import socket
 import struct
 
 class CommunicationHandler():
-    def sendMessage(socket, username, message):
+
+    client, listener = None
+
+    #Setup for the listener server socket and client socket. Also starts the
+    #server, but does not start accepting
+    def __init__:
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        host = listener.gethostname()
+        port = 5555
+        listener.bind((host, port))
+        listener.listen(5)
+
+        client = socket.socket()
+
+    #Sends message to the given socket as the given username.
+    def sendMessage(username, message):
         length = len(message)
 
         remote.send(struct.pack("64s", username.encode()))
         remote.send(struct.pack("I", length))
         remote.send(struct.pack('%ds' % length, message.encode()))
 
+    #Check to see if there is a message available for the listener to recv
+    def checkForMessage():
+        readable = select.select([listener],[],[],0)
+        return len(readable) != 0
+
     #Receive a Message on a given Socket. This is strictly the
     #message in the scope of this program, consisting of 64B name, 4B datalength,
     #data (up to) 2^32
-    def recvMessage(socket):
-        username = struct.unpack('64s', socket.recv(64))[0].replace(b'\0',b'')
-        size = struct.unpack('I', socket.recv(4))[0]
-        data = struct.unpack('%ds' % size, socket.recv(size))[0]
+    def recvMessage():
+        recvsocket = listener.accept()[0]
+        username = struct.unpack('64s', recvsocket.recv(64))[0].replace(b'\0',b'')
+        size = struct.unpack('I', recvsocket.recv(4))[0]
+        data = struct.unpack('%ds' % size, recvsocket.recv(size))[0]
+        recvsocket.close()
 
         return (username, data)
