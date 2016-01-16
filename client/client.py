065231b8412fed893c325d4e832dee612bcd511c
Mitch <mstehula@gmail.com>
2016-01-15 22:41:31 -0500

    Refactoring to remove some of the non essential flow stuff

--- client/client.py
+++ client/client.py
@@ -1,78 +1,25 @@
 #!usr/bin/python
 
-import msvcrt
+from inet import CommHandler
+from ui import UI
+
 import os
 import sys
-import socket
-import struct
-import select
+
 
 print('Welcome to the first iteration of secure chats')
 
 username = str(input('Please enter a username to use: '))
-
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-host = 'localhost'
-port = 5555
-if(len(sys.argv) >= 2):
-    port = int(sys.argv[1])
-listener.bind((host, port))
-listener.listen(5)
-
-remotehost = 'localhost'
-remoteport = 5555
-if(len(sys.argv) >= 3):
-    port = int(sys.argv[2])
-
-remote = socket.socket()
+ch = CommHandler()
+ui = UI(username)
 
 print('Setup complete. Anything you type will be directly transmitted to the remote client')
-outdata = ''
-render = True
-
+print("\r[%s]:" % username,end="")
 while True:
-    readable, writeable, exception = select.select( [listener], [], [], 0)
-    if(len(readable) != 0):
-        (serversocket, address) = listener.accept();
-        inname = struct.unpack('64s', serversocket.recv(64))[0].replace(b'\0',b'')
-        insize = struct.unpack('I', serversocket.recv(4))[0]
-        indata = struct.unpack('%ds' % insize, serversocket.recv(insize))[0]
-        serversocket.close();
+    if(ch.checkForMessage()):
+        username, message = ch.recvMessage()
+        ui.printMessage(username,message)
 
-        print('\r[%s]: %s' % (inname.decode(), indata.decode()),end="\n",flush=True)
-        render = True
-    while msvcrt.kbhit():
-        key = msvcrt.getch();
-        char = ''
-        try:
-            char = key.decode();
-        except UnicodeDecodeError:
-            continue;
-
-        if(key == b'\r'):
-            outsize = len(outdata)
-
-            remote.connect((remotehost, port))
-            remote.send(struct.pack("64s", username.encode()))
-            remote.send(struct.pack("I", outsize))
-            remote.send(struct.pack('%ds' % outsize, outdata.encode()))
-            remote.close()
-
-            remote = socket.socket()
-
-            outdata = ''
-            print('\n',end="", flush=True)
-            render = True
-
-        elif(key == b'\x08'):
-            print('\b \b',end="",flush=True)
-            outdata = outdata[:-1]
-            render = True
-
-        else:
-            outdata += char
-            render = True
-
-    if(render):
-        print('\r[%s]:' % username, outdata,end="",flush=True)
-        render = False
+    readInChars = ui.readInChars()
+    if(readInChars[0]):
+        ch.sendMessage(readInChars[1], readInChars[2])
